af30cc0b9fe5e1109cefde12fe0613dd1d3a9afd
goshiro <goshiro@crooz.co.jp>
2015-12-02 23:59:56 +0900

    12/02

---
+++ Java2.java
@@ -0,0 +1,31 @@
+﻿*/
+@since 2015/12/02
+@:D
+
+
+public class Main {
+	public static void main(String[] args){
+		int a;
+		a = 100;
+		++a;
+		System.out.println(a);
+	}
+}
+
+public class Main{
+	public static void main (String[] args) {
+		int a = 10;
+		int b = 10;
+		System.out.println(++a + 50);
+		System.out.println(b++ + 50 );
+	}
+}
+
+public class Main { 
+  public static void main(String[] args){
+	float f = 3;
+	double d = f;
+	System.out.println(f);
+	System.out.println(d);
+  }
+}
