472e0c854b23df7fc55fa3d3cbe1bd3ca4aac48a
goshiro <goshiro@crooz.co.jp>
2015-12-07 01:34:35 +0900

    Java1

---
+++ Java1.java
@@ -0,0 +1,30 @@
+﻿public class Main {
+	public static void main(String[] args){
+		int fax = 5;
+		doubule tax = 1.08;
+		System.out.println("5万円から4万円");
+		tax = 4;
+		System.out.println(fax*tax+"万円")
+	}
+}
+
+public class Main {
+	public static void main(String [] args){
+		int a = 10;
+		float b = 5.5;
+		float c = a + b;
+		System.out.println("答え：" +c);
+	}
+}
+
+
+
+
+public class Main {
+	public static void main(String[] args){
+		int a ;
+		a = 100;
+		a++;
+		System.out.println(a);
+	}
+}
