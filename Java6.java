6a63cb90b82b0c666227deb45c3c8d1e34b5bf35
goshiro <goshiro@crooz.co.jp>
2015-12-07 01:33:17 +0900

    Java6

---
+++ Java6.java
@@ -0,0 +1,15 @@
+﻿package jp.javadrive;
+
+public class Java4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		boolean tenki = true;
+		if(tenki == true){
+		System.out.println("洗濯をします");
+		System.out.println("散歩に行きます");
+		}else{
+		System.out.println("DVDを観ます");
+		}
+	}
+}
