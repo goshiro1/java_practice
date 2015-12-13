8b24a379d77ad3de3ce7e4fd30a7cbd81b566f2b
goshiro <goshiro@crooz.co.jp>
2015-12-13 17:55:38 +0900

    Java10

---
+++ Java10.java
@@ -0,0 +1,16 @@
+﻿package Java10;
+
+public class Java10 {
+
+	public static void main(String[] args) {
+		int a = 20;
+		int b = 30;
+		int c = 40;
+		int d = 50;
+		
+		int sum = a + b + c + d;
+		int avg = sum / 5;
+		System.out.println("合計点" + sum );
+		System.out.println("平均点" + avg);
+		}
+	}
