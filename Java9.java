5ce88612bc622cb79a26c52b20fdb4ac4af3d8e2
goshiro <goshiro@crooz.co.jp>
2015-12-13 17:55:08 +0900

    Java9

---
+++ Java9.java
@@ -0,0 +1,21 @@
+﻿package Java9;
+
+public class Java9 {
+
+	public static void main(String[] args) {
+		System.out.println("あなたの運勢を占います");
+		int fortune = new java.util.Random().nextInt(4);
+		switch (fortune){
+		
+		case 1: System.out.println("大吉");
+		break;
+		
+		case 2: System.out.println("吉");
+		break;
+		
+		case 3: System.out.println("凶");
+		break;
+		
+		}
+	}
+}
