7a492759853ac9cd9992c79ced301ef46aa8fa14
goshiro <goshiro@crooz.co.jp>
2015-12-18 02:30:05 +0900

    Java11

---
+++ Java11.java
@@ -0,0 +1,17 @@
+﻿package main;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("あなたの体重を入力してください");
+		String bw = new java.util.Scanner(System.in).nextLine();
+		Double byw = Double.valueOf(bw);
+		System.out.println("あなたの身長を入力してください");
+		String ht = new java.util.Scanner(System.in).nextLine();
+		Double het = Double.valueOf(ht);
+		Double hett = het * 100;
+		Double BMI = byw / (hett*hett);
+		Double BMIa = BMI*100000000;
+		System.out.println("あなたのBMIは" + BMIa + "です。");
+	}
+}
